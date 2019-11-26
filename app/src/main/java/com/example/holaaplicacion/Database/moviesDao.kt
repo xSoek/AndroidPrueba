93378a71e77e05cc06f610b05f349eeada945f86
Jorge de Paz Cabañas <jorge.depazcabanas@gmail.com>
2019-11-26 21:09:17 +0100

    solving errors

--- app/src/main/java/com/example/holaaplicacion/Database/moviesDao.kt
+++ app/src/main/java/com/example/holaaplicacion/Database/moviesDao.kt
@@ -14,8 +14,11 @@ interface moviesDao {
     @Query("SELECT * FROM `favourite_movies_database`")
     suspend fun getMovies(): List<FavMovies>
 
-    /*@Query("SELECT * FROM `favourite_movies_database` order by `date added` ")
-    suspend fun getMoviesByDate(): List<FavMovies>*/
+    @Query("SELECT * FROM `favourite_movies_database` order by date_Inserted DESC ")
+    suspend fun getMoviesByDate(): List<FavMovies>
+
+    @Query("SELECT * FROM `favourite_movies_database` order by title ")
+    suspend fun getMoviesByName(): List<FavMovies>
 
     @Query("SELECT count(id) FROM `favourite_movies_database` WHERE id = :movieId")
     suspend fun isInFavourites(movieId: Int): Int
